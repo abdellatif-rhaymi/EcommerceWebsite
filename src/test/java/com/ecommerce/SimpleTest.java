58c09e2bb13c6f24a01cf94c0b8669cdf248b519
abdellatif <abdellatif.rhaymi@gmail.com>
2025-09-25 18:11:09 +0100

    test add

---
+++ src/test/java/com/ecommerce/SimpleTest.java
@@ -0,0 +1,13 @@
+package ecommerce;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SampleTest {
+
+    @Test
+    void testExample() {
+        // Ceci est un test simple qui réussit toujours
+        assertTrue(true);
+    }
+}
