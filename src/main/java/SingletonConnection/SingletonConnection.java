ce95876dc8a800442734a29cdfaf8803f56db847
abdellatif-rhaymi <44725661+abdellatif-rhaymi@users.noreply.github.com>
2025-10-02 09:51:28 +0100

    Update SingletonConnection.java

--- src/main/java/SingletonConnection/SingletonConnection.java
+++ src/main/java/SingletonConnection/SingletonConnection.java
@@ -4,18 +4,21 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class SingletonConnection {
-	private static Connection connection;
-	static {
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			connection=DriverManager.getConnection
-					("jdbc:mysql://localhost:3306/ecommerce","root","root");
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	public static Connection getConnection() {
-		return connection;
-	}
+    private static Connection connection;
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(
+                "jdbc:mysql://mysql:3306/ecommerce?serverTimezone=UTC", 
+                "root", 
+                "root"
+            );
+        } catch (Exception e) {
+            throw new RuntimeException("Erreur de connexion à la base", e);
+        }
+    }
+
+    public static Connection getConnection() {
+        return connection;
+    }
 }
