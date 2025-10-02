17c145e8682442fcd14492d933aa6837f711ba03
abdellatif-rhaymi <44725661+abdellatif-rhaymi@users.noreply.github.com>
2025-10-02 12:05:49 +0100

    Update SingletonConnection.java

--- src/main/java/SingletonConnection/SingletonConnection.java
+++ src/main/java/SingletonConnection/SingletonConnection.java
@@ -2,27 +2,18 @@ package SingletonConnection;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 
 public class SingletonConnection {
     private static Connection connection;
-
     static {
         try {
-            // Utiliser le driver MySQL moderne
             Class.forName("com.mysql.cj.jdbc.Driver");
-
-            // URL JDBC pour Docker Compose : le host est le nom du service MySQL
-            String url = "jdbc:mysql://mysql:3306/ecommerce?serverTimezone=UTC";
-            String user = "root";   // mot de passe défini dans docker-compose
-            String password = "root";
-
-            connection = DriverManager.getConnection(url, user, password);
-
-            System.out.println("Connexion MySQL réussie !");
-        } catch (ClassNotFoundException | SQLException e) {
+            connection = DriverManager.getConnection(
+                "jdbc:mysql://mysql:3306/ecommerce", "root", "root"
+            );
+            System.out.println("Connexion réussie !");
+        } catch (Exception e) {
             e.printStackTrace();
-            throw new RuntimeException("Erreur de connexion à la base de données", e);
         }
     }
 
