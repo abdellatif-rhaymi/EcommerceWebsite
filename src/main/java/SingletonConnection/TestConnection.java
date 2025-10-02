3b35bf9c561a504b04c6aefad06ac78643550a69
abdellatif-rhaymi <44725661+abdellatif-rhaymi@users.noreply.github.com>
2025-10-02 10:53:06 +0100

    Create TestConnection.java

---
+++ src/main/java/SingletonConnection/TestConnection.java
@@ -0,0 +1,19 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class TestConnection {
+    public static void main(String[] args) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conn = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/ecommerce", "root", "root"
+            );
+            if (conn != null && !conn.isClosed()) {
+                System.out.println("Connexion réussie !");
+            }
+            conn.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
