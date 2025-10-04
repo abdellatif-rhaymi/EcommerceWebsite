f0f7ec63ee0efed61b657a5589e8f32f231af2a7
abdellatif-rhaymi <44725661+abdellatif-rhaymi@users.noreply.github.com>
2025-10-04 18:19:24 +0100

    Update SingletonConnection.java

--- src/main/java/SingletonConnection/SingletonConnection.java
+++ src/main/java/SingletonConnection/SingletonConnection.java
@@ -5,14 +5,18 @@ import java.sql.DriverManager;
 
 public class SingletonConnection {
     private static Connection connection;
+
     static {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             connection = DriverManager.getConnection(
-                "jdbc:mysql://mysql:3306/ecommerce", "root", "root"
+                "jdbc:mysql://mysql:3306/ecommerce",
+                "root",
+                "root"
             );
-            System.out.println("Connexion réussie !");
+            System.out.println("✅ Connexion MySQL réussie !");
         } catch (Exception e) {
+            System.out.println("❌ Erreur de connexion à la BD : " + e.getMessage());
             e.printStackTrace();
         }
     }
