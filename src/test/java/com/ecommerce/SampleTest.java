5c713e14da322afe1f8b7b47e071be18d3e9aa73
abdellatif-rhaymi <44725661+abdellatif-rhaymi@users.noreply.github.com>
2025-10-07 00:40:50 +0100

    Update SampleTest.java

--- src/test/java/com/ecommerce/SampleTest.java
+++ src/test/java/com/ecommerce/SampleTest.java
@@ -13,32 +13,11 @@ public class SampleTest {
 
     private static UtilisateurDaoImpl utilisateurDao;
 
-    @BeforeAll
-    public static void init() throws Exception {
-    	utilisateurDao = new UtilisateurDaoImpl();
-        System.out.println("✅ DAO initialisé avec SingletonConnection !");
-    }
+   @BeforeAll
+	public static void init() {
+	    System.out.println("🚀 BeforeAll executed !");
+	    utilisateurDao = new UtilisateurDaoImpl();
+	}
 
-    @AfterAll
-    public static void close() throws Exception {
-        if (connection != null && !connection.isClosed()) {
-            connection.close();
-            System.out.println("🔒 Connexion fermée.");
-        }
-    }
 
-    @Test
-    public void testSaveUtilisateur() {
-        Utilisateur u = new Utilisateur();
-        u.setNom("Abdullah");
-        u.setEmail("abdullah@example.com");
-        u.setMotDePasse("12345678");
-        u.setContact(822828828L);
-        u.setAdresse("Harhoura");
-        u.setRole("client");
-
-        assertDoesNotThrow(() -> {
-            utilisateurDao.saveUtilisateur(u);
-        }, "L'enregistrement d'un utilisateur ne doit pas lever d'exception.");
-    }
 }
