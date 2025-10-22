45f815403802828c11f80da82c78ecd4101a9901
abdellatif-rhaymi <44725661+abdellatif-rhaymi@users.noreply.github.com>
2025-10-22 17:09:45 +0100

    Update TestConnection.java

--- src/main/java/SingletonConnection/TestConnection.java
+++ src/main/java/SingletonConnection/TestConnection.java
@@ -1,14 +1,17 @@
 package SingletonConnection;
+import org.h2.security.SHA256;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class TestConnection {
+    String inputString = System.getenv("SECRET");
+    byte[] key         = inputString.getBytes();
     public static void main(String[] args) {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection conn = DriverManager.getConnection(
-                "jdbc:mysql://mysql-db:3306/ecommerce", "root", "root"
+                "jdbc:mysql://mysql-db:3306/ecommerce", "root", SHA256.getHMAC(key, message);
             );
             if (conn != null && !conn.isClosed()) {
                 System.out.println("Connexion à la base réussie !");
